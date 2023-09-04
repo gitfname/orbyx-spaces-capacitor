d2ab4547a6375b97d8e47c45a09bf374f8bbaf6a
gitfname <newamailnam@gmail.com>
2023-09-04 12:38:08 +0430

    drop-down menu to NavBar component added

--- src/layout-components/NavBar/index.tsx
+++ src/layout-components/NavBar/index.tsx
@@ -3,7 +3,15 @@ import { MobileMenu } from "../MobileMenu"
 import { Button, Search, Text } from "../../components"
 import getBaseUrl from "../../utils/base-url"
 
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react"
+import { MdLogout } from "react-icons/md"
+import { useNavigate } from "react-router-dom"
+import { ApplicationRoutes } from "../../routes"
+
 function NavBar() {
+
+    const navigate = useNavigate()
+
     return (
         <div className="w-full flex items-center justify-between px-4 md:px-8 h-full shadow-sm shadow-black/5">
 
@@ -15,15 +23,100 @@ function NavBar() {
                     Upload
                 </Button>
 
-                <div className="flex items-center gap-x-3 flex-shrink-0">
-                    <img
-                        alt=""
-                        loading="lazy"
-                        src={getBaseUrl() + "/img/14.jpg"}
-                        className="w-11 h-11 object-center object-cover rounded-full shadow-md shadow-black/10"
-                    />
-                    <Text size="subtitle2">Michel</Text>
-                </div>
+                <Menu>
+
+                    <MenuButton className="flex-shrink-0">
+                        <div className="flex items-center gap-x-3">
+                            <img
+                                alt=""
+                                loading="lazy"
+                                src={getBaseUrl() + "/img/14.jpg"}
+                                className="w-11 h-11 object-center object-cover rounded-full shadow-md shadow-black/10"
+                            />
+                            <Text size="subtitle2">Michel</Text>
+                        </div>
+                    </MenuButton>
+
+                    <MenuList
+                        rounded="2xl"
+                        padding="3"
+                    >
+
+                        <MenuItem
+                            onClick={() => navigate(ApplicationRoutes.pages.profile)}
+                            rounded="lg"
+                            py="2"
+                            icon={
+                                <div className="w-7 h-7 rounded-full bg-accent-orange grid place-items-center">
+                                    <img
+                                        alt=""
+                                        src={getBaseUrl() + "/icons/User-White.png"}
+                                        className="w-3.5 h-3.5"
+                                    />
+                                </div>
+                            }
+                        >
+                            <Text size="subtitle2">Profile</Text>
+                        </MenuItem>
+
+                        <MenuItem
+                            onClick={() => navigate(ApplicationRoutes.pages.recentFiles)}
+                            rounded="lg"
+                            py="2"
+                            icon={
+                                <div className="w-7 h-7 rounded-full bg-accent-green grid place-items-center">
+                                    <img
+                                        alt=""
+                                        src={getBaseUrl() + "/icons/Folder-White.png"}
+                                        className="w-3.5 h-3.5"
+                                    />
+                                </div>
+                            }
+                        >
+                            <Text size="subtitle2">Recent Files</Text>
+                        </MenuItem>
+
+                        <MenuItem
+                            onClick={() => navigate(ApplicationRoutes.pages.inviteFriend)}
+                            rounded="lg"
+                            py="2"
+                            icon={
+                                <div className="w-7 h-7 rounded-full bg-accent-pink grid place-items-center">
+                                    <img
+                                        alt=""
+                                        src={getBaseUrl() + "/icons/Invite-Friends-White.png"}
+                                        className="w-3.5 h-3.5"
+                                    />
+                                </div>
+                            }
+                        >
+                            <Text size="subtitle2">Invite a Friend</Text>
+                        </MenuItem>
+
+                        <div className="w-full border-b border-b-brand-3 my-1"></div>
+
+                        <MenuItem
+                            icon={
+                                <div className="w-7 h-7 rounded-full bg-accent-orange grid place-items-center">
+                                    <MdLogout className="w-4 h-4 fill-white" />
+                                </div>
+                            }
+                            rounded="lg"
+                            py="2"
+                            _hover={{
+                                bgColor: "rgba(255,116,116,0.1)"
+                            }}
+                            _focus={{
+                                bgColor: "rgba(255,116,116,0.1)"
+                            }}
+                        >
+                            <Text className="text-accent-red" size="subtitle2">Logout</Text>
+                        </MenuItem>
+
+                    </MenuList>
+
+                </Menu>
+
             </div>
 
             <div className="lg:hidden">
